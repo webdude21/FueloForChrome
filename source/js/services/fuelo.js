7f8e97c6d02a217b932b2a52d502a549c6d5dfec
webdude21 <webdude@webdude.eu>
2015-03-03 15:11:27 +0200

    Added momentjs, and started writing service calls

--- source/js/services/fuelo.js
+++ source/js/services/fuelo.js
@@ -3,13 +3,23 @@ if (!fueloChromeApp) {
 }
 
 fueloChromeApp.services = (function () {
-    "use strict";
+    'use strict';
     var httpRequester = fueloChromeApp.httpRequester,
-        API_KEY = '2aa197c518fe4da';
+        API_KEY = '2aa197c518fe4da',
+        BASE_URL = 'http://fuelo.net/api/',
+        fuel = {
+            gasoline: 'gasoline',
+            diesel: 'diesel',
+            lpg: 'lpg',
+            methane: 'methane'
+        };
 
     return {
-        getAveragePrice: function (){
-
-        }
+        getAveragePrice: function (fuelType, date){
+            fuelType = fuelType || fuel.gasoline;
+            var requestURL = BASE_URL + 'price?key=' + API_KEY + '&fuel=' + fuelType;
+            return httpRequester.getJSON(requestURL);
+        },
+        fuelType: fuel
     }
 }());
