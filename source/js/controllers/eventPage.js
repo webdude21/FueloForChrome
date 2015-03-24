f5ebb54a96a651750b12ff2728a96a0d869aabd0
webdude21 <webdude@webdude.eu>
2015-03-24 23:44:55 +0200

    wrapped the event page in a module

--- source/js/controllers/eventPage.js
+++ source/js/controllers/eventPage.js
@@ -1,44 +1,46 @@
-var persistentStorage = localStorage.favoriteFuelInfo,
-    updateFavoriteFuelInfo = fueloChromeApp.updateFavoriteFuelInformation;
+(function(){
+    var persistentStorage = localStorage.favoriteFuelInfo,
+        updateFavoriteFuelInfo = fueloChromeApp.updateFavoriteFuelInformation;
 
-function drawIcon(text) {
-    chrome.browserAction.setBadgeBackgroundColor({color: [200, 0, 0, 200]});
-    chrome.browserAction.setBadgeText({
-        text: text
-    });
-}
+    function drawIcon(text) {
+        chrome.browserAction.setBadgeBackgroundColor({color: [200, 0, 0, 200]});
+        chrome.browserAction.setBadgeText({
+            text: text
+        });
+    }
 
-function updateIcon() {
-    var savedData = loadSavedData();
-    if (savedData && savedData.fuelType) {
-        if (savedData.cachedValue && dataIsFromToday(savedData.lastUpdated)) {
-            drawIcon(savedData.cachedValue);
-        } else {
-            updateFavoriteFuelInfo(savedData.fuelType).then(function (result) {
-                savedData.cachedValue = result.price;
-                saveChanges(savedData);
-                drawIcon(result.price);
-            });
+    function updateIcon() {
+        var savedData = loadSavedData();
+        if (savedData && savedData.fuelType) {
+            if (savedData.cachedValue && dataIsFromToday(savedData.lastUpdated)) {
+                drawIcon(savedData.cachedValue);
+            } else {
+                updateFavoriteFuelInfo(savedData.fuelType).then(function (result) {
+                    savedData.cachedValue = result.price;
+                    saveChanges(savedData);
+                    drawIcon(result.price);
+                });
+            }
         }
     }
-}
 
-function dataIsFromToday(data){
-    return moment(data).add(1, 'days') > moment().endOf('day');
-}
+    function dataIsFromToday(data){
+        return moment(data).add(1, 'days') > moment().endOf('day');
+    }
 
-function saveChanges(dataToSave) {
-    persistentStorage = JSON.stringify(dataToSave);
-}
+    function saveChanges(dataToSave) {
+        persistentStorage = JSON.stringify(dataToSave);
+    }
 
-function loadSavedData() {
-    return JSON.parse(persistentStorage);
-}
+    function loadSavedData() {
+        return JSON.parse(persistentStorage);
+    }
 
-if (chrome.runtime && chrome.runtime.onStartup) {
-    chrome.runtime.onStartup.addListener(updateIcon);
-    chrome.runtime.onMessage.addListener(function (input) {
-        saveChanges(input);
-        updateIcon();
-    });
-}
+    if (chrome.runtime && chrome.runtime.onStartup) {
+        chrome.runtime.onStartup.addListener(updateIcon);
+        chrome.runtime.onMessage.addListener(function (input) {
+            saveChanges(input);
+            updateIcon();
+        });
+    }
+}());
